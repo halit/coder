1a306c0b94dc18bef166b87e0f8e329afa5d0e49
halit <diqit4ll@gmail.com>
2013-04-15 01:19:24 +0300

    tests added

---
+++ test.c
@@ -0,0 +1,11 @@
+int foo(int x, int y){
+    int sum;
+    sum = x+y;
+    return sum;
+}
+#end#
+
+void bar(){
+    printf("foobar");
+}
+#end#
--- test.py
+++ test.py
@@ -15,7 +15,13 @@ void bar(){
 #end#
 """
 
-parser = parsefile.Parser(string,"#end#")
+#parser = parsefile.Parser(string,"#end#")
+#list = parser.run()
+
+#print list[0]
+
+opener = parsefile.Opener("test.c")
+parser = parsefile.Parser(opener.open(),"#end#")
 list = parser.run()
 
-print list[0]
+print list
